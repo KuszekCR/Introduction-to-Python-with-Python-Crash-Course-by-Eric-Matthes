6e6188a33fd1c69caa1702b550e24e175f3a4543
Cristian Rodrigo Kuszek <kuszek.tech@gmail.com>
2025-09-29 19:14:56 -0300

    Se modificaron un par de cosas en el archivo hello_world.py y se dejó un comentario.

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+
+#1 print("Hello world!")
+
+#2 message = "Hello Python world!"
+# print(message)
+
+message2 = "Hello Python Crash Course world"
+print(message2)
+
+#You can change the value of a variable in your program at any time, 
+#and Python will always keep track of its current value.
+
