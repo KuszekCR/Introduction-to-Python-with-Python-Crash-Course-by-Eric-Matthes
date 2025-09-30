2071814b1f41d3acd38108a13ef0b0f1980f83bc
Cristian Rodrigo Kuszek <kuszek.tech@gmail.com>
2025-09-30 09:10:04 -0300

    For some reason the previous commits were made in spanish, so from now on they are gonna be made in english for convenience. We created a name.py file and made some changes to the hello_world.py file.

--- hello_world.py
+++ hello_world.py
@@ -7,15 +7,15 @@
 #3 message2 = "Hello Python Crash Course world"
 # print(message2)
 
-message3 = "Hello Python Crash Course reader"
-print(mesage3)
+# message3 = "Hello Python Crash Course reader"
+# print(mesage3)
 
 # It will appear: NameError: name 'mesage3' is not defined. Did you mean: 'message3'?
 
-# When an error occurs in your program, the Python interpreter does its best to help you figure out 
-# where the problem is. The interpreter provides a traceback when a program cannot run successfully.
-# A traceback is a record of where the interpreter ran into trouble when trying to execute your code. 
+'''When an error occurs in your program, the Python interpreter does its best to help you figure out 
+where the problem is. The interpreter provides a traceback when a program cannot run successfully.
+A traceback is a record of where the interpreter ran into trouble when trying to execute your code.'''
 
-# You can change the value of a variable in your program at any time, 
-# and Python will always keep track of its current value.
+'''You can change the value of a variable in your program at any time, 
+and Python will always keep track of its current value.'''
 
---
+++ name.py
@@ -0,0 +1,11 @@
+name = "rodrigo kuszek"
+print(name.title())
+print(name.upper())
+name2 = "ADA LOVELACE"
+print(name2.lower())
+
+'''The method title() appears after the variable in the print() call. 
+A method is an action that Python can perform on a piece of data. The dot 
+(.) after name in name.title() tells Python to make the title() method act on 
+the variable name. You can apply a variety of methods to a str variable, like upper, lower, etc.'''
+
