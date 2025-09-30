cfa1ac4f9c449383d16a86a6e58c0d42e285face
Cristian Rodrigo Kuszek <kuszek.tech@gmail.com>
2025-09-29 21:09:54 -0300

    Añadimos un error a propósito sumado a otros comentarios al respecto.

--- hello_world.py
+++ hello_world.py
@@ -4,9 +4,18 @@
 #2 message = "Hello Python world!"
 # print(message)
 
-message2 = "Hello Python Crash Course world"
-print(message2)
+#3 message2 = "Hello Python Crash Course world"
+# print(message2)
 
-#You can change the value of a variable in your program at any time, 
-#and Python will always keep track of its current value.
+message3 = "Hello Python Crash Course reader"
+print(mesage3)
+
+# It will appear: NameError: name 'mesage3' is not defined. Did you mean: 'message3'?
+
+# When an error occurs in your program, the Python interpreter does its best to help you figure out 
+# where the problem is. The interpreter provides a traceback when a program cannot run successfully.
+# A traceback is a record of where the interpreter ran into trouble when trying to execute your code. 
+
+# You can change the value of a variable in your program at any time, 
+# and Python will always keep track of its current value.
 
